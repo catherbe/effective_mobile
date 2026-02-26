9a5130b0f4b52c771e401ae6ebb3b2fffcefb770
mira 9 <mira9@MacBook-Air-mira.local>
2026-02-26 09:51:38 +0300

    test

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Hello from Effective Mobile!")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def log_message(self, format, *args):
+        return
+
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+server.serve_forever()
